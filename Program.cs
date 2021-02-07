5fc480ccb68abc584db46c5fee779f4e1ff5df9d
Tannor Wallace <tannor.wallace@gmail.com>
2021-02-07 16:28:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomCardPicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number of cards You want!:");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a Valid Number!!");
+
+            }
+        }
+    }
+}
